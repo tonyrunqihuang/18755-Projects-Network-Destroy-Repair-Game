a390046a0f2fe5f7d4b00d01b1ec7b338328108e
307Kashyap <90943140+307Kashyap@users.noreply.github.com>
2022-11-11 18:13:57 -0500

    Add files via upload

---
+++ algorithm/defense.py
@@ -0,0 +1,59 @@
+import random
+import numpy as np
+import networkx as nx
+
+
+class Defense():
+    def __init__(self, G, p, num_edges):
+        self.G = G
+        self.p = p
+        self.num_edges = num_edges
+
+    def random_defense(self):
+
+        nodes = list(self.G.nodes())
+
+        if not nodes:
+            return self.G
+
+        else:
+            i = 0
+            
+            while(1):
+
+                random_nodes = random.sample(nodes, 2)
+                if self.G.has_edge(random_nodes[0], random_nodes[1]) == False:
+                    self.G.add_edge(random_nodes[0], random_nodes[1])
+                    i+=1
+                if i == int(self.p * self.num_edges):
+                    break
+
+            return self.G
+            
+
+    def smart_defense(self):
+
+
+        degree = sorted(self.G.degree, key=lambda x: x[1], reverse=True)
+        node_select = degree[:(int(len(degree) * self.p))]
+        node_not_select = degree[(int(len(degree) * self.p)):]
+        
+        i = self.G.number_of_edges()
+        
+        while(1):
+          
+          for node in node_select:
+            
+             random_node = random.sample(node_not_select, 1)
+
+             if self.G.has_edge(node[0], random_node[0][0]) == False:
+                
+                self.G.add_edge(node[0], random_node[0][0])
+                i+=1
+                if  i == self.num_edges:
+                  break
+        
+          if  i == self.num_edges:
+             break
+
+        return self.G
